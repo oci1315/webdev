5928a2a4378c134c68625d58c317ec35b5abd216
Cédric Donner <cedonner@gmail.com>
2015-03-07 19:17:58 +0000

    ajout données produits

--- corrige/generation_html/donnee.py
+++ corrige/generation_html/donnee.py
@@ -37,3 +37,20 @@ commissions2 = [
                 ],
         ],
 ]
+
+
+class TableExo(object):
+    
+    headers = ['Identifiant', 'Titre', 'Type de reliure', 'Prix unitaire (CHF)']
+    products = [ ('100', 'Le guide des vins 2005', 'cartonné', '50'),
+                 ('200', 'Dieux du stade', 'cartonné', '100'),
+                 ('300', 'Rupture de contrat', 'broché', '10'),
+                 ('400', 'Pars vite et reviens tard', 'broché', '12'),
+                 ('500', 'Panique au collège', 'broché', '7'),
+                 ('600', 'Marketing management', 'spirales', '120'),
+                 ('700', "L'art de la guerre", 'cartonné', '12'),
+                 ('800', 'Excel pour le business et la finance', 'spirales', '75'),
+                 ('900', '10 ans de leçon de séduction', 'cartonné', '60'),
+                 ('1000', 'Autant en emporte le vent', 'cuir', '150')
+                ]
+                
